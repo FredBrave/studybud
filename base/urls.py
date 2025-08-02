8ee11337b1752338c6f89e3ef43e0680c4e53768
FredBrave <freddy.bravo.robalino@gmail.com>
2025-08-02 17:58:08 -0500

    Adding news models

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room_page/<str:id>/', views.room, name='room'),
+    path('room/<str:id>/', views.room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Designs with me'},
-    {'id':3, 'name':'Fronetend developers'}
-]
+#rooms = [
+#   {'id':1, 'name':'Lets learn python!'},
+#   {'id':2, 'name':'Designs with me'},
+#   {'id':3, 'name':'Fronetend developers'}
+#]
+
 def home(request):
+    rooms = Room.objects.all()
     contex = {'rooms': rooms}
     return render(request, 'base/home.html', contex)
 
 def room(request, id):
-    room = None
-    for i in rooms:
-        if i['id'] == int(id):
-            room = i
+    room = Room.objects.get(id=id)
     context = {'room': room}
     return render(request, 'base/room.html', context)

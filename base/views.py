06d73b9b4dcd972bbe0f67af4666729d1a3b0fc8
FredBrave <freddy.bravo.robalino@gmail.com>
2025-08-02 15:56:49 -0500

    Adding templates

--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Designs with me'},
+    {'id':3, 'name':'Fronetend developers'}
+]
 def home(request):
-    return HttpResponse('Home Page')
+    contex = {'rooms': rooms}
+    return render(request, 'base/home.html', contex)
 
-def room(request):
-    return HttpResponse('Room')
+def room(request, pk):
+    return render(request, 'base/room.html')

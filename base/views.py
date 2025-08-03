6955390fbd948cd747f30edc438e3c9a220bd543
FredBrave <freddy.bravo.robalino@gmail.com>
2025-08-02 19:26:43 -0500

    feat: search function added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 #rooms = [
 #   {'id':1, 'name':'Lets learn python!'},
@@ -9,8 +10,14 @@ from .forms import RoomForm
 #]
 
 def home(request):
-    rooms = Room.objects.all()
-    contex = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) |
+                                Q(description__icontains=q)
+                                )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    contex = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', contex)
 
 def room(request, id):

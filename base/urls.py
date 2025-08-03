67410ff740d8ecbae3756d98aed668de813fbf3c
FredBrave <freddy.bravo.robalino@gmail.com>
2025-08-03 14:02:33 -0500

    feat: user profile added

--- base/urls.py
+++ base/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
     path('', views.home, name='home'),
     path('room/<str:id>/', views.room, name='room'),
+    path('profile/<str:id>/', views.userProfile, name='user-profile'),
     path('create_room/', views.create_room, name='create-room'),
     path('update_room/<str:id>', views.updateRoom, name='update-room'),
     path('delete_room/<str:id>', views.deleteRoom, name='delete-room'),
